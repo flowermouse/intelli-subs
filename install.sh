9ee07f0134ca89ef3d1809adcdf887dc97ae9b2e
flowermouse <haushulizi@gmail.com>
2025-12-14 21:57:37 +0800

    update

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ python new_transcribe.py $input_video -o ${input_video%.mp4}.srt
 # Linux/MacOS: ~/.cache/huggingface/hub/models--nvidia--parakeet-tdt-0.6b-v2/
 # 2.47 GB
 
+# whisper
+whisper 021.mp3 --language Chinese --model turbo --output_format srt
+# 模型运行时自动下载
+# Linux/MacOS: ~/.cache/whisper/
+# 1.51 GB
+
 # ffmpeg
 conda install -c conda-forge ffmpeg -y
 
@@ -19,7 +25,8 @@ demucs --two-stems=vocals ${input_video%.mp4}.mp3
 # 80.2 MB
 
 # voxcpm
+pip intall voxcpm
 # 第一次运行时会自动下载模型文件，也可以手动下载：
-hf download openbmb/VoxCPM-0.5B
-# Linux/MacOS: ~/.cache/huggingface/hub/models--openbmb--VoxCPM-0.5B
-# 1.4 GB
+hf download openbmb/VoxCPM1.5
+# Linux/MacOS: ~/.cache/huggingface/hub/models--openbmb--VoxCPM1.5/
+# 2 GB
--- voxcpm.sh
+++ voxcpm.sh
@@ -1,12 +1,12 @@
 pip install voxcpm
 
 # 第一次运行时会自动下载模型文件，也可以手动下载：
-hf download openbmb/VoxCPM-0.5B
+hf download openbmb/VoxCPM1.5
 
 # cli
 voxcpm --text "VoxCPM is an innovative end-to-end TTS model from ModelBest, designed to generate highly expressive speech." --output out.wav
 
-
+# f5-tts
 pip install f5-tts
 
 f5-tts_infer-cli --model F5TTS_v1_Base \
